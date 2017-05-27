ffeaaac5205353db77d2c83d8b5aa8027a531479
Michał Niegrzybowski <michal.niegrzybowski@gmail.com>
2017-05-27 12:47:45 +0200

    init

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'typescript-vs2017'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.microsoft.com/download/7/0/A/70A6AC0E-8934-4396-A43E-445059F430EA/2.3.3-TS-release-dev14update3-20170519.1/TypeScript_SDK.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs   = "/quiet /norestart /log `"$env:TEMP\chocolatey\$($packageName)\$($packageName).Install.log`""
+  validExitCodes= @(0)
+
+  softwareName  = 'TypeScript Tools for Microsoft Visual Studio 2017 2.3.3.0'
+  checksum      = '289CC7183F808BC2C9198E7994FE8CFF5CCC459E06A6567FF131F72C59DB649A'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
