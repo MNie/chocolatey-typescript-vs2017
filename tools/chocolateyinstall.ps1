ab9ab7e8428897c38132d9a7033e579b025f9acd
Michal Niegrzybowski <michal.niegrzybowski@ihs.com>
2017-08-18 07:07:04 +0200

    update sdk to 2.5.0 RC

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'typescript-vs2017'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/7/0/A/70A6AC0E-8934-4396-A43E-445059F430EA/2.4.1-TS-release-dev14update3-20170626.1/TypeScript_SDK.exe'
+$url        = 'http://download.microsoft.com/download/7/0/A/70A6AC0E-8934-4396-A43E-445059F430EA/2.5.0-TS-release-dev14update3-20170816.2/TypeScript_SDK.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -14,7 +14,7 @@ $packageArgs = @{
   validExitCodes= @(0)
 
   softwareName  = 'TypeScript Tools for Microsoft Visual Studio 2017 2.4.1.0'
-  checksum		= 'f1c291a71d364816ccd9ae42fdfd073c5f1e6632ae1cfdd5b77d5506e2d27e3f'
+  checksum		= '4cd1c0f11bcfd3928ee4e894cb7e2635d5d72ca0'
   checksumType  = 'sha256'
 }
 
