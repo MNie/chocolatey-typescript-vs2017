db4f63206218e7ebbab03608376d9631dc347870
Michał Niegrzybowski <michal.niegrzybowski@gmail.com>
2018-06-05 07:35:33 +0200

    2.9.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'typescript-vs2017'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/7/0/A/70A6AC0E-8934-4396-A43E-445059F430EA/2.8.4-TS-release-dev14update3-20180515.1/TypeScript_SDK.exe'
+$url        = 'https://download.microsoft.com/download/7/0/A/70A6AC0E-8934-4396-A43E-445059F430EA/2.9.1-TS-release-dev14update3-20180525.2/TypeScript_SDK.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,8 +13,8 @@ $packageArgs = @{
   silentArgs   = "/quiet /norestart /log `"$env:TEMP\chocolatey\$($packageName)\$($packageName).Install.log`""
   validExitCodes= @(0)
 
-  softwareName  = 'TypeScript Tools for Microsoft Visual Studio 2017 2.8.4'
-  checksum		= '02cebc5f21e7cf839caf6bd51c2bbad3e64240211f153b5ceda9482eee962736'
+  softwareName  = 'TypeScript Tools for Microsoft Visual Studio 2017 2.9.1'
+  checksum		= '4d8676b68fc4cf834cf700f461719516aacd70d6156549f4a2ecaf42a20c86d3'
   checksumType  = 'sha256'
 }
 
